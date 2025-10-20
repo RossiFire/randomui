e9f6b018e7f4026eaff83f6977dd1c1990f0d52c
Daniele Rossino <fratsavio.dr@gmail.com>
2025-10-21 00:21:20 +0200

    feat: handled-response utility

--- lib/handled-response.ts
+++ lib/handled-response.ts
@@ -1,17 +1,19 @@
-export type HandledResponse<T> = {
-    data: T;
-    error: null;
-} | {
-    data: null;
-    error: string;
-}
+export type HandledResponse<T> =
+  | {
+      data: T;
+      error: null;
+    }
+  | {
+      data: null;
+      error: string;
+    };
 
 export const ok = <T>(data: T): HandledResponse<T> => ({
-    data,
-    error: null
-})
+  data,
+  error: null,
+});
 
 export const err = <T>(error: string): HandledResponse<T> => ({
-    data: null,
-    error
-})
+  data: null,
+  error,
+});
